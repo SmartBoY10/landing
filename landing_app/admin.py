86f9927d1c61eacd3bf4568ae118379f0d81c359
SmartBoY10 <qurol.abdujalilov99@gmail.com>
2022-03-08 16:14:17 +0500

    Admin panel

--- landing_app/admin.py
+++ landing_app/admin.py
@@ -9,5 +9,27 @@ class WorkingTimeAdmin(admin.ModelAdmin):
 
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ("id", "full_name")
+    list_display = ("id", "full_name", "_get_days", "_get_total")
     list_display_links = ("id", "full_name")
+
+    def _get_days(self, obj):
+        hours = Employee.objects.get(id=obj.id).an_hour_a_day.all()
+        day = {}
+        for hour in hours:
+            day[f"{hour.date}"] = f"{hour.hour}/{hour.norm_hour}"
+        return day
+
+    def _get_total(self, obj):
+        hours = Employee.objects.get(id=obj.id).an_hour_a_day.all()
+        hours_worked = 0
+        norm_hour = 0
+        for day in hours:
+            hours_worked += day.hour
+            norm_hour += day.norm_hour
+
+        total = {hours_worked: norm_hour}
+        return total
+
+
+    _get_days.short_description = "Days"
+    _get_total.short_description = "Total"
--- landing_app/models.py
+++ landing_app/models.py
@@ -1,11 +1,13 @@
-from datetime import date
 from django.db import models
 
 
 class WorkingTime(models.Model):
-    date = models.DateField(verbose_name="День", auto_now_add=True)
+    date = models.DateField(verbose_name="День")
     norm_hour = models.FloatField(verbose_name="Часы по нормативу", default=8)
     hour = models.FloatField(verbose_name="Часы отработана")
+
+    def __str__(self):
+        return f"{self.date}"
 
 
 class Employee(models.Model):
@@ -13,4 +15,4 @@ class Employee(models.Model):
     an_hour_a_day = models.ManyToManyField(WorkingTime, verbose_name="Час в день")
 
     def __str__(self):
-        return self.full_name
+        return self.full_name      
